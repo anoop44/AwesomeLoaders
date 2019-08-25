74994cf5992e3839b1a2687d26758fd5d61a2e2a
Anoop <anoopvvs@gmail.com>
2019-08-25 22:51:29 +0530

    Four Circle loader demo added

--- app/src/main/java/ss/anoop/awesomeloaders/demo/LauncherActivity.kt
+++ app/src/main/java/ss/anoop/awesomeloaders/demo/LauncherActivity.kt
@@ -3,6 +3,7 @@ package ss.anoop.awesomeloaders.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_launcher.*
+import ss.anoop.awesomeloaders.FourCircleLoader
 import ss.anoop.awesomeloaders.PinWheelLoader
 import ss.anoop.awesomeloaders.TwoArcLoader
 
@@ -18,7 +19,8 @@ class LauncherActivity : AppCompatActivity() {
     private fun getLoaderList(): List<AwesomeLoaderData> {
         return listOf(
             AwesomeLoaderData(TwoArcLoader(this), "Two Arc Loader"),
-            AwesomeLoaderData(PinWheelLoader(this), "Pin Wheel Loader")
+            AwesomeLoaderData(PinWheelLoader(this), "Pin Wheel Loader"),
+            AwesomeLoaderData(FourCircleLoader(this), "Four Circle Loader")
         )
     }
 }
